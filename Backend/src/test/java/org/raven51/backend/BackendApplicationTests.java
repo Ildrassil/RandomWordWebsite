3e49ac765ada95edf35602c0e0b11102bbf95500
Ildrassil <144778160+Ildrassil@users.noreply.github.com>
2024-04-21 21:49:37 +0200

    Getting Started

---
+++ Backend/src/test/java/org/raven51/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.raven51.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
